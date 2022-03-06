a1b1098c07cc6279247e944b20a62d87aeb33ae1
Girish Madnani <girishmadnani05@gmail.com>
2022-03-06 19:40:23 +0000

    validators

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Intern = require("./lib/intern.js");
 
 const fs = require("fs");
 const inquirer = require("inquirer");
+const emailValidator = require('email-validator');
 
 const path = require("path");
 
@@ -50,24 +51,44 @@ function generateQuestions() {
           type: "input",
           name: "managerName",
           message: "Manager's name:",
+          validate: (value) => {
+            if (value) {
+                return true
+            } else { return "Please enter manager's name." }
+          },
         },
 
         {
-          type: "number",
+          type: "input",
           name: "managerId",
           message: "Manager's employee ID number:",
+          validate: (value) => {
+            if (value && !isNaN(value)) {
+                return true
+            } else { return "Please enter manager's id number." }
+        },
         },
 
         {
           type: "input",
           name: "managerEmail",
           message: "Manager's email address:",
+          validate: (value) => {
+            if (emailValidator.validate(value)) {
+                return true
+            } else { return 'Please enter a valid email address.' }
+        },
         },
 
         {
           type: "input",
           name: "managerOfficeNumber",
           message: "Manager's office number:",
+          validate: (value) => {
+            if (value && !isNaN(value)) {
+                return true
+            } else { return "Please enter manager's office number." }
+        },
         },
       ])
       .then((answers) => {
@@ -89,24 +110,44 @@ function generateQuestions() {
           type: "input",
           name: "engineerName",
           message: "Engineer's name:",
+          validate: (value) => {
+            if (value) {
+                return true
+            } else { return "Please enter engineer's name." }
+        },
         },
 
         {
-          type: "number",
+          type: "input",
           name: "engineerId",
           message: "Engineer's employee ID number:",
+          validate: (value) => {
+            if (value && !isNaN(value)) {
+                return true
+            } else { return "Please enter engineer's id number." }
+        },
         },
 
         {
           type: "input",
           name: "engineerEmail",
           message: "Engineer's email address:",
+          validate: (value) => {
+            if (emailValidator.validate(value)) {
+                return true
+            } else { return 'Please enter a valid email address.' }
+        },
         },
 
         {
           type: "input",
           name: "engineerGithub",
           message: "Engineer's GitHub username:",
+          validate: (value) => {
+            if (value) {
+                return true
+            } else { return "Please enter engineer's GitHub." }
+        },
         },
       ])
       .then((answers) => {
@@ -128,24 +169,44 @@ function generateQuestions() {
           type: "input",
           name: "internName",
           message: "Intern's name:",
+          validate: (value) => {
+            if (value) {
+                return true
+            } else { return "Please enter intern's name." }
+        },
         },
 
         {
-          type: "number",
+          type: "input",
           name: "internId",
           message: "Intern's employee ID number:",
+          validate: (value) => {
+            if (value && !isNaN(value)) {
+                return true
+            } else { return "Please enter intern's id number." }
+        },
         },
 
         {
           type: "input",
           name: "internEmail",
           message: "Intern's email address:",
+          validate: (value) => {
+            if (emailValidator.validate(value)) {
+                return true
+            } else { return 'Please enter a valid email address.' }
+        },
         },
 
         {
           type: "input",
           name: "internSchool",
           message: "Intern attend:",
+          validate: (value) => {
+            if (value) {
+                return true
+            } else { return "Please enter the name of school." }
+        },
         },
       ])
       .then((answers) => {
